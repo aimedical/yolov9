d779e19cab25223a7c13ea283fe5c5c871079261
Hikaru Araki <hikaru.araki@ai-ms.com>
2025-02-03 18:25:43 +0900

    Fix bugs.

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -233,8 +233,8 @@ class Loggers():
             file = self.save_dir / 'results.csv'
             n = len(x) + 1  # number of cols
             s = '' if file.exists() else (('%20s,' * n % tuple(['epoch'] + self.keys)).rstrip(',') + '\n')  # add header
-            with open(file, 'a') as f:
-                f.write(s + ('%20.5g,' * n % tuple([epoch] + vals)).rstrip(',') + '\n')
+            # with open(file, 'a') as f:
+            #     f.write(s + ('%20.5g,' * n % tuple([epoch] + vals)).rstrip(',') + '\n')
 
         if self.tb:
             for k, v in x.items():
@@ -274,8 +274,8 @@ class Loggers():
 
     def on_train_end(self, last, best, epoch, results):
         # Callback runs on training end, i.e. saving best model
-        if self.plots:
-            plot_results(file=self.save_dir / 'results.csv')  # save results.png
+        # if self.plots:
+        #     plot_results(file=self.save_dir / 'results.csv')  # save results.png
         files = ['results.png', 'confusion_matrix.png', *(f'{x}_curve.png' for x in ('F1', 'PR', 'P', 'R'))]
         files = [(self.save_dir / f) for f in files if (self.save_dir / f).exists()]  # filter
         self.logger.info(f"Results saved to {colorstr('bold', self.save_dir)}")
